6296cdf794135a409667f22ff87aa051f036c450
Timmythecreator <timurledos@gmail.com>
2024-07-28 19:06:44 +0200

    uploading a correct version

--- malloc.c
+++ malloc.c
@@ -1,4 +1,4 @@
-#include "smalloc.h"
+#include "malloc.h"
 #include <stdlib.h>
 #include <string.h>
 
--- malloc.h
+++ malloc.h
@@ -1,21 +1,33 @@
-#ifndef SMALLOC_H
-#define SMALLOC_H
+#ifndef MALLOC_H
+#define MALLOC_H
 
 #include <stddef.h>
 
 #ifdef _WIN32
   #ifdef SMALLOC_EXPORTS
-    #define SMALLOC_API __declspec(dllexport)
+    #define MALLOC_API __declspec(dllexport)
   #else
-    #define SMALLOC_API __declspec(dllimport)
+    #define MALLOC_API __declspec(dllimport)
   #endif
 #else
-  #define SMALLOC_API
+  #define MALLOC_API
 #endif
 
-SMALLOC_API void *malloc(size_t size);
-SMALLOC_API void free(void *block);
-SMALLOC_API void *calloc(size_t num, size_t nsize);
-SMALLOC_API void *realloc(void *block, size_t size);
+typedef char ALIGN[16];
+
+union header {
+    struct {
+        size_t size;
+        unsigned is_free;
+        union header *next;
+    } s;
+    ALIGN stub;
+};
+typedef union header header_t;
+
+MALLOC_API void *malloc(size_t size);
+MALLOC_API void free(void *block);
+MALLOC_API void *calloc(size_t num, size_t nsize);
+MALLOC_API void *realloc(void *block, size_t size);
 
 #endif
